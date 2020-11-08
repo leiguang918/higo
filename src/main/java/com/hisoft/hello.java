731ca8393840bd2c5eb5137098594ccb20332f98
leiguang <2507426098@qq.comj>
2020-11-08 16:42:21 +0800

    ff

--- src/main/java/com/hisoft/hello.java
+++ src/main/java/com/hisoft/hello.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class hello {
     @RequestMapping("/hello")
     public String run(){
+        System.out.println("jj");
         System.out.println("come in");
         return "hello";
     }
