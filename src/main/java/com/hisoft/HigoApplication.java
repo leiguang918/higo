916cf3e1d7d76b1b613230f984238fd762542f0a
leiguang <2507426098@qq.comj>
2020-11-08 16:39:30 +0800

    Initial commit

---
+++ src/main/java/com/hisoft/HigoApplication.java
@@ -0,0 +1,13 @@
+package com.hisoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HigoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HigoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/hisoft/hello.java
@@ -0,0 +1,13 @@
+package com.hisoft;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class hello {
+    @RequestMapping("/hello")
+    public String run(){
+        System.out.println("come in");
+        return "hello";
+    }
+}
